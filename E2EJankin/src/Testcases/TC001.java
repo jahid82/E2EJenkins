da1965d7d9020f48a597f1d13f9d41190f23fde7
jahidul islam <jahid_humanist@yahoo.com>
2018-05-29 20:44:50 -0400

    Create automation Project

---
+++ E2EJankin/src/Testcases/TC001.java
@@ -0,0 +1,30 @@
+package Testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC001 {
+	
+	@Test
+	public void testcase1() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\jahid\\Documents\\Selenium_work\\Chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://opensource.demo.orangehrmlive.com/");
+		
+   String title=driver.getTitle();
+		
+		if(title.equalsIgnoreCase("orangeHRM"))
+		{
+		System.out.println("Landed in the right page");	
+		}
+		else
+		{
+			System.out.println("wrong site");
+		}
+		
+	driver.quit();
+		
+	}
+
+}
